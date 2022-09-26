b8bf048b2bde8d63b5edb34fc1ab8a5d7c0f0e98
Admin1 <a89086829981@gmail.com>
2022-09-26 16:21:57 +0300

    first

---
+++ app/src/main/java/com/ak87/suminjavatoolsshop/DrillDetailActivity.java
@@ -0,0 +1,14 @@
+package com.ak87.suminjavatoolsshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DrillDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drill_detail);
+    }
+}
